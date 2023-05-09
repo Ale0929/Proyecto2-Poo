c1aeb931aa540fbf557bbd477da44068a908b3c0
Gabrielm27 <100320795+Gabrielm27@users.noreply.github.com>
2023-05-09 04:41:35 -0600

    Update controlador.java
    agregacion de funciones principales

--- prueba/src/Controlador/controlador.java
+++ prueba/src/Controlador/controlador.java
@@ -2,13 +2,57 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Controlador;
+package Control;
 
+import Modelo.TIPOTriangulo;
+import Modelo.Triangulo;
+import Vista.EscogerOperacion;
+import Vista.Galton;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
- * @author alejandropachecosanchez
+ * @author hgabr
  */
-public class controlador {
+
+public class Controlador {
+    private Triangulo triangulo;
+    public static ArrayList<JLabel> estructura = EscogerOperacion.labels;
+    public static ArrayList<ArrayList<Integer>> triangleDimension = EscogerOperacion.triangleDimension;
+    public static JPanel trianglePanel = EscogerOperacion.trianglePanel;
+    public static JPanel mainPanel = EscogerOperacion.mainPanel;
+    public static int size = EscogerOperacion.sizeTriangulo;
+
+    public Controlador() {}
+    
+    /**
+    *
+    *
+    */
+    public void crearTriangulo() {
+        triangulo = new Triangulo(size, estructura) {
+            @Override
+            public void Funcion() {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+        };
+    }
+    
+    public void cambiarTriangulo(TIPOTriangulo tipo) {
+        triangulo = Factory.getNewTriangulo(tipo, size, estructura); //revisar si es necesario el casting
+    }
+    
+    public void ejecutarFuncion(TIPOTriangulo tipo) {
+        cambiarTriangulo(tipo);
+        if (triangulo instanceof Modelo.Simetria)
+            trianglePanel.paint(trianglePanel.getGraphics());
+        triangulo.Funcion();
+    }
+    
+    public void empezarSimulacion() {
+        new Galton(size);
+    }
     
 }
